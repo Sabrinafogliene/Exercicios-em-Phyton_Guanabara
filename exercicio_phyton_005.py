00ade2202bd08fe1588f24d80d16635b187494bd
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-04-27 17:44:00 -0300

    exercicio_005

---
+++ exercicio_phyton_005.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s)) #usando 3 varáveis
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1))) #usando apenas uma variável
