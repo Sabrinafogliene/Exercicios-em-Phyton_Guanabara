8c329c4950282d2c06ef450dfd60e5e8ccdc66d0
Sabrinafogliene <sabrina.castilho4@gmail.com>
2025-06-03 20:08:50 -0300

    Update exercicio_phyton_002.py

--- exercicio_phyton_002.py
+++ exercicio_phyton_002.py
@@ -1,5 +1,12 @@
 #Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+#nome = input('Qual seu nome? ')
+
+#print('Seja muito bem vindo,', nome)
+#print('É um prazer te conhecer, {}!'.format(nome))
+
+#-----------------------------------------------------------------------
+
 nome = input('Qual seu nome? ')
 
 print('Seja muito bem vindo,', nome)
-print('É um prazer te conhecer, {}!'.format(nome))
+print('É um prazer te conhecer {}!'.format(nome))
