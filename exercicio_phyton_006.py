e9a41f0bdbb2e03c9834863033a930180f96d1c4
Sabrinafogliene <sabrina.castilho4@gmail.com>
2025-06-04 16:37:02 -0300

    Update exercicio_phyton_006.py

--- exercicio_phyton_006.py
+++ exercicio_phyton_006.py
@@ -1,7 +1,18 @@
+
 #Crie um algoritmo que leia um número e mostre o seu dobro, o seu triplo e a sua raiz quadrada.
+#n = int(input('Digite um número: '))
+#d = n * 2
+#t = n * 3
+#r = n ** (1/2)
+#print ('O dobro de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} é igual a {:.2f}.'.format(n, d, n, t, n, r)) 
+#4 variáveis
+#print('O dobro de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} é igual a {:.2f}.'.format(n, (n*2), n, (n*3), n, (n**(1/2))))
+#uma variável
+
+#=============================================
 n = int(input('Digite um número: '))
 d = n * 2
 t = n * 3
-r = n ** 2
-print ('O dobro de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} é igual a {}.'.format(n, d, n, t, n, r)) #4 variáveis
-print ('O dobro de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} é igual a {}.'.format(n, (n*2), n, (n*3), n, (n**2))) #uma variável
+r = n ** (1/2)
+print('O dobro de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} vale {:.2f}.'.format(n, d, n, t, n, r))
+print('O dobro de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} vale {:.2f}.'.format(n, (n*2), n, (n*3), n, (n**(1/2))))
