1d6408d805121714c087a61dc31d7a9b15c12c7b
Sabrinafogliene <sabrina.castilho4@gmail.com>
2025-06-04 14:13:46 -0300

    Update exercicio_phyton_005.py

--- exercicio_phyton_005.py
+++ exercicio_phyton_005.py
@@ -1,6 +1,15 @@
 #Faça um programa que leia um número e mostre na tela o seu sucessor e o seu antecessor.
+#n = int(input('Digite um número: '))
+#a = n - 1
+#s = n + 1
+#print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s)) #usando 3 varáveis
+#print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1))) #usando apenas uma variável
+
+#==============================================================================================
+
 n = int(input('Digite um número: '))
 a = n - 1
 s = n + 1
-print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s)) #usando 3 varáveis
-print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1))) #usando apenas uma variável
+
+print('Analisando o número {}, seu antecessor é {} e seu sucessor é {}.'.format(n, a, s))
+print('Analisando o número {}, seu sucessor é {} e seu antecessor é {}.'.format(n, n+1, n-1))
