bc0da649266548799ae0af6acffe445623f9c753
Sabrinafogliene <sabrina.castilho4@gmail.com>
2025-06-04 20:38:48 -0300

    Update exercicio_phyton_011.py

--- exercicio_phyton_011.py
+++ exercicio_phyton_011.py
@@ -1,7 +1,14 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua àrea e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m².
+#larg = float(input('Largura da parede: '))
+#alt = float(input('Altura da parede: '))
+#área = larg * alt
+#print('Sua parede tem a dimensão de {}x{} e sua àrea é de {}m².'.format(larg, alt, área))
+#tinta = área / 2
+#print('Para pintar essa parede, você precisará de {}l de tinta'.format(tinta))
+#===================================
 larg = float(input('Largura da parede: '))
 alt = float(input('Altura da parede: '))
-área = larg * alt
-print('Sua parede tem a dimensão de {}x{} e sua àrea é de {}m².'.format(larg, alt, área))
-tinta = área / 2
-print('Para pintar essa parede, você precisará de {}l de tinta'.format(tinta))
+area = larg * alt
+print('Sua parede tem a dimensão de {} x {} e a sua área é de {}m².'.format(larg, alt, area))
+tinta = area/2
+print('Para pintar essa parede, será necessário {}l de tinta'.format(tinta))
