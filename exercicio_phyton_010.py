035f98ae1b9205b0a7feb943063634b36609748a
Sabrinafogliene <sabrina.castilho4@gmail.com>
2025-06-04 19:36:30 -0300

    Update exercicio_phyton_010.py

--- exercicio_phyton_010.py
+++ exercicio_phyton_010.py
@@ -1,7 +1,13 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares e quantos euros ela pode comprar.
 
-real = float(input('Quanto dinheiro você tem na carteira? R$'))
-dolar = real / 5.19
-euro = real / 5.54
+#real = float(input('Quanto dinheiro você tem na carteira? R$'))
+#dolar = real / 5.19
+#euro = real / 5.54
+#print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
+#print('Com R${:.2f} você pode comprar EUR{:.2f}'.format(real, euro))
+#===========================================================
+real = float(input('Quanto dinheiro você tem na carteira: R$'))
+dolar = real / 5.63
+euro = real / 6.44
 print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
 print('Com R${:.2f} você pode comprar EUR{:.2f}'.format(real, euro))
